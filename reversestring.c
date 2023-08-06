ea653bb9acedf487efc48cd7d5141c33a2ef5be6
chiranjeevits <140326552+chiranjeevits@users.noreply.github.com>
2023-08-06 22:54:08 +0530

    Update reversestring.c

--- reversestring.c
+++ reversestring.c
@@ -1,27 +1,22 @@
-#include <stdio.h>
-int reversestring()
-{
-   char s[1000], r[1000];
-   int begin, end, count = 0;
-
-   printf("Input a string\n");
-   gets(s);
-
-   // Calculating string length
-
-   while (s[count] != '\0')
-      count++;
-
-   end = count - 1;
-
-   for (begin = 0; begin < count; begin++) {
-      r[begin] = s[end];
-      end--;
-   }
-
-   r[begin] = '\0';
-
-   printf("%s\n", r);
-
-   return 0;
+#include <stdio.h>  
+#include <string.h>  
+int string_reverse()  
+{  
+char str1[50], temp; // declare and initialize the size of string array.  
+int i = 0, j =0;  
+printf (" Enter a string to be reversed: ");  
+scanf( "%s", str1);  
+j = strlen (str1) - 1;  // get the length of the string  
+// use while loop to define the condition  
+while ( i < j)   
+{  
+// use temp variable to store the characters of str1  
+temp = str1[j];  
+str1[j] = str1[i];  
+str1[i] = temp;  
+i++; // i incremented by 1  
+j--; // j is decremented by 1  
+}  
+printf (" The reversed of the string: %s", str1);  
+return 0;  
 }
